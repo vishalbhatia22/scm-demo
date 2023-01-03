ccfad470a64beadbba8a07a1205b8b38360c76cb
cwmd1500 <71892925+cwmd1500@users.noreply.github.com>
2023-01-03 12:22:34 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+
+document.getElementById("r0c0").onclick = function () {
+   alert("W1 Course Intro and FTP Client descriptions");
+}
+
+document.getElementById("r0c1").onclick = function () {
+   alert("W2 Client Applications descriptions");
+}
+
+document.getElementById("r0c2").onclick = function () {
+   alert("W3 Source Code Management (SCM) descriptions");
+}
+
+document.getElementById("r0c3").onclick = function () {
+   alert("W4 Service Providers over the Internet descriptions");
+}
+
+document.getElementById("r0c4").onclick = function () {
+   alert("W5 Web Protocols descriptions");
+}
+
+document.getElementById("r1c0").onclick = function () {
+   alert("W6 Development Practices and Tools descriptions");
+}
+
+document.getElementById("r1c1").onclick = function () {
+   alert("W7 Web Publishing descriptions");
+}
+
+document.getElementById("r1c2").onclick = function () {
+   alert("W8 Web Medias descriptions");
+}
+
+document.getElementById("r1c3").onclick = function () {
+   alert("W9 Computer Languages descriptions");
+}
+
+document.getElementById("r1c4").onclick = function () {
+   alert("W10 Language Ext. - Snippet, Lib. and FW descriptions");
+}
+
+
+document.getElementById("r2c0").onclick = function () {
+   alert("W11 Collaborative Development descriptions");
+}
+
+document.getElementById("r2c1").onclick = function () {
+   alert("W12 Web File Exchanges descriptions");
+}
+
+document.getElementById("r2c2").onclick = function () {
+   alert("W13 File Exchanges: AJAX/XHR and JSON descriptions");
+}
+
+document.getElementById("r2c3").onclick = function () {
+   alert("W14 Final Test descriptions");
+}
